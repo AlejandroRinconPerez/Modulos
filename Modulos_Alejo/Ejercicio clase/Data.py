201fa1c98f48ea8983b522af1dbfc6c4b4fc94a1
AlejandroRinconPerez <alejandro.ingenieria@gmail.com>
2024-05-29 07:14:09 -0500

    menu

---
+++ Modulos_Alejo/Ejercicio clase/Data.py
@@ -0,0 +1,3 @@
+partipantes = {"1095824299":
+                            {"Nombre": "Alejandro", "Edad":29, "Carrera": "atletismo ","posicion":[100],  "Departamenbto":'santander' }
+}
---
+++ Modulos_Alejo/Ejercicio clase/Main.py
@@ -0,0 +1,7 @@
+
+from Funcion_menu import*
+
+submenu()
+
+
+
