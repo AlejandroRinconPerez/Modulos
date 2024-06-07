07b06aee2a8486a0c343e6ba7d70587c58f5d39d
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2024-06-06 21:55:30 -0500

    dvsv

---
+++ Archivos de estudio/CVS .py
@@ -0,0 +1,32 @@
+#Comma-Separated Values CSV 
+
+
+#Paso 1 ( importamos CSV  dentro de Python es una libreria pre instalada )
+
+import json
+
+# with open('Archivo_preuba.csv',mode = 'r', newline = '' )as file:
+#     reader = csv.reader(file)
+#     for i in reader:
+#         print(i)
+
+def Save_at_json (diccionarios):
+    global Route
+    Route = "lear_about.json"
+    
+    
+    
+    
+
+Dict = {}
+elementos = {}
+elementos ["Nombre "]= "Alejandro" 
+elementos['Apellido'] = "Rincon"
+elementos[ "Numero de telefono"]= "3178876402"
+Dict["Info personal"] = {}
+Dict["Info personal"] = elementos
+
+
+
+
+
